c34bc0edb05ff44afe22a9620ac873dba599e0e3
ElijahKB <b.kaneza@gmail.com>
2024-11-06 09:44:43 +0000

    new

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
+"""" Top Ten Limit"""
 import requests
 
-def top_ten(subreddit):
-    """Function that queries the Reddit API."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        data = response.json().get('data', {}).get('children', [])
-        for post in data:
-            print(post.get('data', {}).get('title', None))
+def top_ten(subreddit):
+    """"top ten"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
+        .format(subreddit)
+
+    res = requests.get(url,
+                       headers={
+                           'User-Agent': 'Mozilla/5.0'})
+
+    if res.status_code != 200:
+        print(None)
     else:
-        print("None")
+        json_response = res.json()
+        posts = json_response.get('data').get('children')
+        [print(post.get('data').get('title')) for post in posts]
