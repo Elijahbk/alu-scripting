9fc7fd2dbcaf8cda4951615c253dd9128f7102ac
ElijahKB <b.kaneza@gmail.com>
2024-11-06 09:33:53 +0000

    new

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
+"""Module for top_ten function"""
 import requests
 
-def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "custom-user-agent-for-subreddit-subscriber-check"}
-    
-    try:
-        # Make a request to the Reddit API
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        # Check if the request was successful and if the subreddit exists
-        if response.status_code == 200:
-            data = response.json()
-            return data["data"]["subscribers"]
-        else:
-            # If status is not 200, it could be an invalid subreddit
-            return 0
-    except Exception as e:
-        # Any unexpected exceptions are handled here
-        print(f"An error occurred: {e}")
-        return 0
+
+def top_ten(subreddit):
+    """Function that queries the Reddit API."""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
+        .format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json().get('data').get('children')
+        for post in data:
+            print(post.get('data').get('title'))
+    else:
+        print(None)
